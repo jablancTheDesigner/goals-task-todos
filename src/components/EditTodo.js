bb8f63cd1a1d78da4435d5ba4f71f362c716a3de
Jahmal Blanchard <45908841+jablancTheDesigner@users.noreply.github.com>
2022-03-05 15:50:34 -0500

    add local storage

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,36 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function EditTodo(props) {
-    const [newName, setNewName] = useState("")
-    function handleChange(e) {
-        setNewName(e.target.value)
-    }
+  const [newName, setNewName] = useState("");
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.editTask(props.id, newName)
-        props.setIsEditing(false)
-        setNewName("")
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    props.setIsEditing(false);
+    setNewName("");
+  }
 
-    return (
-        <form className="stack-small" onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label className="todo-label" htmlFor={props.id}>
-                    New name for {props.name}
-                </label>
-                <input id={props.id} className="todo-text" type="text" value={newName} onChange={handleChange} />
-            </div>
-            <div className="btn-group">
-                <button type="button" className="btn todo-cancel" onClick={() => props.setIsEditing(false)}>
-                    Cancel
-                    <span className="visually-hidden"> renaming {props.name}</span>
-                </button>
-                <button type="submit" className="btn btn__primary todo-edit">
-                    Save
-                    <span className="visually-hidden"> new name for {props.name}</span>
-                </button>
-            </div>
-        </form>
-    )
+  return (
+    <form className="stack-small text-center mb-4" onSubmit={handleSubmit}>
+      <div className="grid grid-cols-6">
+        <input
+          id={props.id}
+          className="todo-text p-3 col-span-full md:col-span-4"
+          type="text"
+          onChange={handleChange}
+          placeholder={`New name for "${props.name}"`}
+        />
+        <button
+          type="button"
+          className="btn todo-cancel p-3 bg-red-700 text-white hover:bg-red-800 col-span-3 md:col-span-1"
+          onClick={() => props.setIsEditing(false)}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="btn btn__primary todo-edit p-3 bg-sky-700 text-white hover:bg-sky-800 col-span-3 md:col-span-1"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,93 +1,120 @@
-import React, { useState } from 'react'
-import TodoForm from './TodoForm'
-import Todo from './Todo';
-import { nanoid } from 'nanoid';
+import React, { useEffect, useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+import { nanoid } from "nanoid";
 
 export default function TodoApp() {
-    const [tasks, setTasks] = useState([]);
-    const headerText = `${tasks.length} ${tasks.length === 1 ? 'task' : 'tasks'}`;
-    const taskList = tasks.length > 0 ? createList() : createOpenMsg();
+  const [tasks, setTasks] = useState([]);
+  const headerText = `${tasks.length} ${tasks.length === 1 ? "task" : "tasks"}`;
+  const taskList = tasks.length > 0 ? renderTasks() : noTasks();
 
-    function createList() {
-        const list = tasks.map(task => {
-            return (
-                <Todo name={task.name}
-                    key={task.id}
-                    id={task.id}
-                    toggleCompleted={toggleCompleted}
-                    editTask={editTask}
-                    deleteTask={deleteTask}
-                    completed={task.completed} />
-            )
-        })
-        return list;
-    }
-
-    function createOpenMsg() {
+  function renderTasks() {
+    const list = tasks
+      .sort((a, b) => {
+        return b.created - a.created;
+      })
+      .map((task) => {
         return (
-            <h1 className="text-4xl bold fixed left-0 right-0 inset-y-2/4 -translate-y-2/4 text-lime-800">No Tasks</h1>
-        )
-    }
+          <Todo
+            name={task.name}
+            key={task.id}
+            id={task.id}
+            toggleCompleted={toggleCompleted}
+            editTask={editTask}
+            deleteTask={deleteTask}
+            completed={task.completed}
+          />
+        );
+      });
+    return list;
+  }
 
-    function addTask(name) {
-        const newTask = { id: 'task_' + nanoid(), name: name, completed: false };
-        setTasks([
-            ...tasks,
-            newTask
-        ]);
-    }
-
-    function toggleCompleted(id) {
-        const updatedTasks = tasks.map(task => {
-            if (task.id === id) {
-                return { ...task, completed: !task.completed };
-            }
-            return task;
-        })
-        setTasks(updatedTasks)
-        console.log(updatedTasks)
-    }
-
-    function deleteTask(id) {
-        const remaingTasks = tasks.filter(task => {
-            return task.id !== id;
-        })
-        setTasks(remaingTasks)
-        console.log(remaingTasks)
-    }
-
-    function editTask(id, newName) {
-        const editedTasks = tasks.map(task => {
-            if (task.id === id) {
-                return { ...task, name: newName }
-            }
-            return task;
-        })
-        setTasks(editedTasks)
-        console.log(editedTasks)
-    }
-
+  function noTasks() {
     return (
-        <div className="todo-app flex flex-col">
-            <div className="p-4 text-center bg-lime-800/[0.8]">
-                <div className="container mx-auto">
-                    <h1 className="todo-app__title text-5xl font-bold text-white"><em>my</em>Todos</h1>
-                    <p className="todo-app__sub-title text-white text-md">Made by Jahmal &amp; Built in React</p>
-                </div>
-            </div>
+      <h1 className="text-4xl bold fixed left-0 right-0 inset-y-2/4 -translate-y-2/4 text-lime-800 text-center">
+        No Tasks
+      </h1>
+    );
+  }
 
-            <div className="text-center">
-                <div className="container mx-auto">
-                    <div className="todo-app__task-list p-4 md:w-5/6 mx-auto">
-                        {taskList}
-                    </div>
-                </div>
-            </div>
+  function addTask(name) {
+    const newTask = {
+      id: "task_" + nanoid(),
+      name: name,
+      completed: false,
+      created: Date.now(),
+    };
+    storeTask([...tasks, newTask]);
+  }
 
-            <div className="mt-auto fixed bottom-0 left-0 right-0 z-10">
-                <h2 className="todo-app__task-header text-center text-2xl font-medium text-white bg-lime-800/[0.8] p-2">{headerText}</h2>
-                <TodoForm addTask={addTask} />
-            </div>
+  function toggleCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    storeTask(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remaingTasks = tasks.filter((task) => task.id !== id);
+    storeTask(remaingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    storeTask(editedTasks);
+  }
+
+  function storeTask(data) {
+    setTasks(data);
+    localStorage.setItem("todos", JSON.stringify(data));
+  }
+
+  function getStoredTasks() {
+    const reference = localStorage.getItem("todos");
+    let storedTodos = [];
+    // if reference exists
+    if (reference) {
+      // converts back to array and store it in todos array
+      storedTodos = JSON.parse(reference);
+    }
+    setTasks(storedTodos);
+  }
+
+  useEffect(() => {
+    getStoredTasks();
+  }, []);
+
+  return (
+    <div className="todo-app flex flex-col">
+      <div className="todo-app__header p-4 text-center bg-lime-800/[0.8]">
+        <div className="container mx-auto">
+          <h1 className="todo-app__title text-5xl font-bold text-white">
+            <em>my</em>Todos
+          </h1>
+          <p className="todo-app__sub-title text-white text-md">
+            Made by Jahmal &amp; Built in React
+          </p>
         </div>
-    )
+      </div>
+
+      <div className="todo-app__task-list p-4 md:w-5/6 grow overflow-y-auto md:mx-auto">
+        <div className="container mx-auto">{taskList}</div>
+      </div>
+
+      <div className="todo-app__form">
+        <h2 className="todo-app__task-header text-center text-2xl font-medium text-white bg-lime-800/[0.8] p-2">
+          {headerText}
+        </h2>
+        <TodoForm addTask={addTask} />
+      </div>
+    </div>
+  );
 }
